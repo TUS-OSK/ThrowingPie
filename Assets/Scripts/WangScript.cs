adcae8fd8a3244aa9596fdc44af52184e5b61931
yuya sato <mrfool38@gmail.com>
2016-12-25 04:03:47 +0900

    fix: Wang delete problem

--- Assets/Scripts/WangScript.cs
+++ Assets/Scripts/WangScript.cs
@@ -50,10 +50,14 @@ public class WangScript : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D c)
     {
+        if (c.tag != "PlayerPie") return;
+        hitPoint--;
         // 弾の削除
         Destroy(c.gameObject);
-
-        // プレイヤーを削除
-        Destroy(this.gameObject);
+        if (hitPoint <= 0)
+        {
+            // プレイヤーを削除
+            Destroy(this.gameObject);
+        }
     }
 }
