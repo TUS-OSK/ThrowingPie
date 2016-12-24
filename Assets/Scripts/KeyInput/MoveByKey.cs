ebf103ab70fffbc38f5f76f41408bd920a044080
yuya sato <mrfool38@gmail.com>
2016-12-24 15:22:43 +0900

    add: player move

---
+++ Assets/Scripts/KeyInput/MoveByKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByKey : MonoBehaviour {
+
+	// Use this for initialization
+	Transform transform;
+	float speed = 0.3f;
+
+	void Start () {
+		this.transform = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dx = Input.GetKey(KeyCode.D) ? +1 : Input.GetKey(KeyCode.A) ? -1 : 0;
+		float dy = Input.GetKey(KeyCode.W) ? +1 : Input.GetKey(KeyCode.S) ? -1 : 0;
+		float normalizeScale = Mathf.Sqrt(dx*dx + dy*dy);
+		float speedScale = speed/normalizeScale;
+		transform.position = new Vector3(
+			transform.position.x + dx*speedScale,
+			transform.position.y + dy*speedScale,
+			0
+		);
+	}
+}
