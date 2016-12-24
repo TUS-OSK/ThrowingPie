24f6964f01bd4f1350044101c4453b84c0c1ab49
FickleFox <izzet0927@gmail.com>
2016-12-24 15:29:00 +0900

    wang増える

--- Assets/Scripts/WangScript.cs
+++ Assets/Scripts/WangScript.cs
@@ -6,15 +6,17 @@ using UnityEngine.EventSystems;
 public class WangScript : MonoBehaviour {
 
     Transform tf;
+    PieThrower pt;
 	// Use this for initialization
 	void Start () {
         tf = this.GetComponent<Transform>();
+        pt = this.GetComponent<PieThrower>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        tf.position = new Vector3(tf.position.x + Random.value - 0.5f, tf.position.y + Random.value - 0.5f, tf.position.z);
-	}
+        pt.Move(transform.up * -1);
+    }
     
     void OnMouseDown()
     {
