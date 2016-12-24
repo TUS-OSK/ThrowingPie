7bfdff339a498fc01b95debe63559c055b206e7e
yuya sato <mrfool38@gmail.com>
2016-12-24 14:24:51 +0900

    merge

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
+	float x;
+	float y; 
+
 	// Use this for initialization
 	void Start () {
-		
+		x = 0;
+		y = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Transform target = this.GetComponent<Transform>();
+		x += 0.1f;
+		y += 0.1f;
+		target.position = new Vector3(x,y,0);
 	}
 }
